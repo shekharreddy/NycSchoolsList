9fadce23d7bb37fdf8a88b395693abde1e18cc82
shekharnareddy <reddy.anddev@gmail.com>
2024-02-05 13:39:18 -0500

    Added click action for below 2. Selecting a school should show additional information about the school. At the very least, display the overview_paragraph

--- app/src/main/java/com/nsr/nycschools/MainActivity.kt
+++ app/src/main/java/com/nsr/nycschools/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
@@ -17,17 +20,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModelProvider
 import com.nsr.nycschools.ui.theme.NYCSchoolsTheme
 import com.nsr.nycschools.viewmodel.NycSchoolsViewModel
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.style.TextAlign
-import androidx.lifecycle.Observer
 import com.nsr.nycschools.network.ApiHelper
 import com.nsr.nycschools.network.NYCAPIClient
-import com.nsr.nycschools.network.NYCApiService
 import com.nsr.nycschools.viewmodel.NycSchoolsUiModel
 import com.nsr.nycschools.viewmodel.NycViewModelFactory
 
@@ -81,22 +84,38 @@ private fun ShowSchoolsList(
 
 @Composable
 fun SchoolItem(item: NycSchoolsUiModel, modifier: Modifier = Modifier) {
-    Column(Modifier.padding(all = 16.dp),
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    Column(Modifier.padding(all = 16.dp)
+        .clickable {
+            expanded = !expanded
+        }
+        .animateContentSize(
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioLowBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )
+        ,
         verticalArrangement = Arrangement.spacedBy(8.dp)
 
     ) {
+
         Text(
             text = item.dbn.orEmpty(),
-            modifier = modifier
-        )
+            modifier = modifier,
+
+            )
         Text(
             text = item.schoolName.orEmpty(),
             modifier = modifier
         )
-        Text(
-            text = item.overView.orEmpty(),
-            modifier = modifier
-        )
+        if(expanded) {
+            Text(
+                text = item.overView.orEmpty(),
+                modifier = modifier
+            )
+        }
     }
 
 }
@@ -106,9 +125,10 @@ fun SchoolItem(item: NycSchoolsUiModel, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     NYCSchoolsTheme {
         SchoolItem(NycSchoolsUiModel(
-            dbn = "sample DBN",
-            schoolName = "",
-            overView = ""
+            dbn = "02M260",
+            schoolName = "Clinton School Writers & Artists",
+            overView = "Students who are prepared for college must have " +
+                    "an education that encourages them to take risks as they produce and perform. "
         )
         )
     }
